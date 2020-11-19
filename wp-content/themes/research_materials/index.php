690e292bddc3903069d00eca1f9a18e4e6291af5
erica-garga <64044554+erica-garga@users.noreply.github.com>
2020-11-19 20:37:30 +0100

    theme php

--- wp-content/themes/research_materials/index.php
+++ wp-content/themes/research_materials/index.php
@@ -1,1 +1,36 @@
-<h1>Research Materials Theme</h1>
+
+
+<?php 
+	$sources_query = new WP_Query([
+		'post_type' => 'source',
+		'post_per_page' => -1,
+	]);
+?>
+
+<?php if ($sources_query->have_posts()): ?>
+	<div class="sources">
+		<?php while($sources_query->have_posts()): $sources_query->the_post(); ?>
+			
+			<div class="source">
+				<div class='date'><?php echo get_the_date(); ?></div>
+				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+				<div>
+					<?php $type_terms = wp_get_post_terms ($post->ID, 'source_type'); ?>
+					<?php foreach($type_terms as $term): ?>
+						<span class="tag"><?php echo $term->name; ?></span>
+					<?php endforeach; ?>
+				</div>
+
+				<div>
+					<?php the_field('author_name'); ?>
+				</div>
+
+			</div>
+
+			<hr />
+
+		<?php endwhile ?>
+	</div>
+
+<?php endif; ?>
--- wp-content/themes/research_materials/single.php
+++ wp-content/themes/research_materials/single.php
@@ -1,1 +1,64 @@
 <h1><?php the_title(); ?></h1>
+<div><?php the_content(); ?></div>
+
+<div>
+	Author: <?php the_field('author_name'); ?>
+</div>
+
+<div>
+	Position: <?php the_field('author_position'); ?>
+</div>
+
+<div>
+	URL: <a href="<?php the_field('url'); ?>">this is the link you can call it whatever you want</a>
+</div>
+
+<br>
+
+
+
+<?php if (have_rows('quotes')): ?>
+	<div class="quotes">
+		<?php while (have_rows('quotes')): the_row(); ?>
+			<div><?php the_sub_field('quote_text'); ?></div>
+		<?php endwhile; ?>
+	</div>
+<?php endif; ?>
+
+
+
+
+<?php if (have_rows('media')): ?>
+	<div class="media">
+		<?php while (have_rows('media')): the_row(); ?>
+
+			<?php 
+				$media_type = get_sub_field('media_type');
+				$image = get_sub_field('image');
+				$file = get_sub_field('file');
+				$embed = get_sub_field('embed');
+			?>
+
+			<?php if($media_type == 'image'): ?>
+				<div>
+					<img src="<?php echo $image['sizes']['medium']; ?>"/>
+				</div>
+
+			<?php elseif($media_type == 'file'): ?>
+				<div>
+					<a href="<?php echo $file['url'];?>"><?php echo $file['name']; ?></a>
+				</div>
+			
+			<?php elseif($media_type == 'embed'): ?>
+				<div>
+					<?php echo $embed; ?>
+				</div>
+
+			<?php endif; ?>
+			
+
+
+		<?php endwhile; ?>
+	</div>
+<?php endif; ?>
+
